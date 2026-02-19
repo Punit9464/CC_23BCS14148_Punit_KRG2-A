eb6ae46eb8698439e7e6bccd2b0eb564e36e5bbc
Punit <punitkumar8728@gmail.com>
2026-02-19 22:01:15 +0530

    Add Solution class with sumBitDifferences method
    Implement a solution to calculate the sum of bit differences in an array.

---
+++ Experiment 4/main.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int sumBitDifferences(vector<int>& arr) {
+        int ans = 0, n = arr.size();
+
+        // traverse over all bits
+        for (int i = 0; i < 32; i++) {
+            // count number of elements with i'th bit set
+            int count = 0;
+            for (int j = 0; j < n; j++)
+                if ((arr[j] & (1 << i)))
+                    count++;
+
+            // Add "count * (n - count) * 2" to the answer
+            ans += (count * (n - count) * 2);
+        }
+
+        return ans;
+    }
+};
